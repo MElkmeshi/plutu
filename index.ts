8b55521f4da8eeacaf004f11c34c2a463da34dad
MElkmeshi <elkmeshi2002@gmail.com>
2024-10-03 22:27:20 +0300

    update dependencies: remove @types/express, add zod and zod-express-middleware

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import dotenv from "dotenv";
 import crypto from "crypto";
 import { ParsedQs } from "qs";
+import { z } from "zod";
+import { validateRequest } from "zod-express-middleware";
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,7 +17,6 @@ const plutu_access_token = process.env.PLUTU_ACCESS_TOKEN;
 if (!plutu_api_key || !plutu_secret_key || !plutu_access_token) {
   throw new Error("Please provide all the required environment variables");
 }
-
 function verifyPlutuCallbackHash(
   parameters: ParsedQs,
   secretKey: string,
@@ -62,49 +64,163 @@ function verifyPlutuCallbackHash(
     .digest("hex")
     .toUpperCase();
 
-  if (
-    !crypto.timingSafeEqual(
-      Buffer.from(generatedHash),
-      Buffer.from(hashFromCallback)
-    )
-  ) {
-    return false;
-  }
-  return true;
+  return crypto.timingSafeEqual(
+    Buffer.from(generatedHash),
+    Buffer.from(hashFromCallback)
+  );
 }
 
-app.post("/", async (req, res) => {
-  if (
-    req.body.payment_method === "localbankcards" ||
-    req.body.payment_method === "tlync"
-  ) {
-    const form_data = new FormData();
-    form_data.append("amount", req.body.amount);
-    form_data.append("invoice_no", req.body.invoice_no);
-    form_data.append("return_url", `${APP_URL}/return`);
-    form_data.append("lang", "ar");
-    if (req.body.payment_method === "tlync") {
-      form_data.append("mobile_number", req.body.mobile_number);
-      form_data.append("callback_url", req.body.callback_url);
-    }
-    const response = await axios.post(
-      `https://api.plutus.ly/api/v1/transaction/${req.body.payment_method}/confirm`,
-      form_data,
-      {
-        headers: {
-          "x-api-key": plutu_api_key,
-          lang: "ar",
-          Authorization: `Bearer ${plutu_access_token}`,
-        },
-      }
-    );
-    res.send(response.data.result.redirect_url);
-  } else {
-    res.send("Payment method not supported");
-  }
-});
+const InitiatepaymentSchema = z.discriminatedUnion("payment_method", [
+  z.object({
+    payment_method: z.literal("sadadapi"),
+    mobile_number: z.string(),
+    birth_year: z.string(),
+    amount: z.number(),
+  }),
+  z.object({
+    payment_method: z.literal("edfali"),
+    mobile_number: z.string(),
+    amount: z.number(),
+  }),
+]);
 
-app.get("/return", (req, res) => {
+app.post(
+  "/payment/initiate",
+  validateRequest({
+    body: InitiatepaymentSchema,
+  }),
+  async (req, res) => {
+    const form_data = new FormData();
+    form_data.append("lang", "ar");
+    for (const key in req.body) {
+      // @ts-ignore
+      form_data.append(key, req.body[key]);
+    }
+    try {
+      const response = await axios.post(
+        `https://api.plutus.ly/api/v1/transaction/${req.body.payment_method}/verify`,
+        form_data,
+        {
+          headers: {
+            "x-api-key": plutu_api_key,
+            lang: "ar",
+            Authorization: `Bearer ${plutu_access_token}`,
+          },
+        }
+      );
+      res.status(response.status).send(response.data);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          res.status(500).send({
+            message: "No response received from the server",
+            error: error.message,
+          });
+        } else {
+          res.status(500).send({
+            message: "Error setting up the request",
+            error: error.message,
+          });
+        }
+      } else {
+        res.status(500).send({
+          message: "An unexpected error occurred",
+          error: String(error),
+        });
+      }
+    }
+  }
+);
+
+const confirmPaymentSchema = z.discriminatedUnion("payment_method", [
+  z.object({
+    payment_method: z.literal("localbankcards"),
+    amount: z.number(),
+    invoice_no: z.string(),
+    return_url: z.string(),
+  }),
+  z.object({
+    payment_method: z.literal("tlync"),
+    amount: z.number(),
+    invoice_no: z.string(),
+    callback_url: z.string(),
+    mobile_number: z.string(),
+    return_url: z.string(),
+  }),
+  z.object({
+    payment_method: z.literal("sadadapi"),
+    process_id: z.string(),
+    invoice_no: z.string(),
+    code: z.number(),
+    amount: z.number(),
+  }),
+  z.object({
+    payment_method: z.literal("edfali"),
+    process_id: z.string(),
+    invoice_no: z.string(),
+    code: z.number(),
+    amount: z.number(),
+  }),
+  z.object({
+    payment_method: z.literal("mpgs"),
+    amount: z.number(),
+    invoice_no: z.string(),
+    return_url: z.string(),
+  }),
+]);
+
+app.post(
+  "/payment/confirm",
+  validateRequest({ body: confirmPaymentSchema }),
+  async (req, res) => {
+    const form_data = new FormData();
+    form_data.append("lang", "ar");
+    for (const key in req.body) {
+      // @ts-ignore
+      form_data.append(key, req.body[key]);
+    }
+    try {
+      const response = await axios.post(
+        `https://api.plutus.ly/api/v1/transaction/${req.body.payment_method}/confirm`,
+        form_data,
+        {
+          headers: {
+            "x-api-key": plutu_api_key,
+            lang: "ar",
+            Authorization: `Bearer ${plutu_access_token}`,
+          },
+        }
+      );
+      res.status(response.status).send(response.data);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          res.status(500).send({
+            message: "No response received from the server",
+            error: error.message,
+          });
+        } else {
+          res.status(500).send({
+            message: "Error setting up the request",
+            error: error.message,
+          });
+        }
+      } else {
+        res.status(500).send({
+          message: "An unexpected error occurred",
+          error: String(error),
+        });
+      }
+    }
+  }
+);
+
+app.get("/payment/return", (req, res) => {
+  console.log(req.query);
   if (verifyPlutuCallbackHash(req.query, plutu_secret_key)) {
     console.log("Redirect : Payment successful");
     res.send("Payment successful");
@@ -114,7 +230,8 @@ app.get("/return", (req, res) => {
   }
 });
 
-app.use("/webhook", (req, res) => {
+app.use("/payment/webhook", (req, res) => {
+  console.log(req.body);
   if (verifyPlutuCallbackHash(req.body, plutu_secret_key, true)) {
     console.log("Webhook : Payment successful");
   } else {
